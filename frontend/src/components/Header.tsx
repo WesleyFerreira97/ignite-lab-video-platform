4ab42bb88a682d478d86037133348f1e4fdec749
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-06-22 20:58:17 -0300

    Def theme colors

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,8 @@ import { Logo } from './Logo'
 
 export function Header() {
     return (
-        <div className='w-full h-[72px] bg-gray-700 flex justify-center align-center'>
-            <h1 className='text-5xl leading-none bg-slate-400 m'>Header</h1>
+        <div className='w-full py-5 bg-gray-700 flex justify-center align-center'>
+            <Logo />
         </div>
     )
 }
