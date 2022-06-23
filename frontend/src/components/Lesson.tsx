bad7d521c0a5e9f61d9fbd65cc301061df462735
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-06-23 00:51:55 -0300

    Create query gql in db

--- frontend/src/components/Lesson.tsx
+++ frontend/src/components/Lesson.tsx
@@ -1,26 +1,46 @@
-import { CheckCircle } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
 import React from 'react'
 
-export function Lesson() {
+interface LessonProps {
+    title: string
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+
+}
+
+export function Lesson(props: LessonProps) {
+    const isLessonAvailable = false;
+
+
     return (
         <a href="" className=''>
             <span className='text-gray-300'>
-                Domingo • 20 de junho • 19h00
+                {props.availableAt.toString()}
             </span>
 
             <div className='p-4 mt-2 border border-gray-500 rounded'>
                 <header className='flex justify-between'>
-                    <span className='flex items-center gap-2 text-sm text-blue-500 font-medium'>
-                        <CheckCircle size={20} />
-                        Conteúdo liberado
-                    </span>
+
+                    {isLessonAvailable ? (
+                        <span className='flex items-center gap-2 text-sm text-blue-500 font-medium'>
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className='flex items-center gap-2 text-sm text-orange-500 font-medium'>
+                            <Lock size={20} />
+                            Em Breve
+                        </span>
+                    )}
+
                     <span className='text-xs rounded py-[2px] px-2 text-white border border-green-300 font-bold'>
-                        Ao Vivo
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
                 </header>
 
-                <strong className='text-gray-200 mt-5 font-bold block'>
-                    Abertura do evento Ignite labs
+                <strong className='text-gray-200 mt-4 font-bold block'>
+                    {props.title}
                 </strong>
             </div>
 
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,33 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import { Lesson } from './Lesson'
 
+const GET_LESSONS_QUERY = gql`
+    query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+        id
+        lessonType
+        publishedAt
+        slug
+        title
+    }
+}`
+
+interface GetLessonsQueryResponse {
+    lessons: {
+        id: string;
+        title: string
+        slug: string;
+        availableAt: Date;
+        lessonType: 'live' | 'class';
+    }[]
+}
+
 export function Sidebar() {
+    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
+    console.log(data);
+
     return (
         <div className='w-[348px] bg-gray-700 p-6 border-l border-gray-600'>
             <span className='w-full font-bold text-2xl mb-6 pb-6 border-b border-gray-500 block'>
@@ -9,11 +35,17 @@ export function Sidebar() {
             </span>
 
             <div className='flex flex-col gap-8'>
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
+                {data?.lessons.map(lesson => {
+                    return (
+                        <Lesson
+                            key={lesson.id}
+                            title={lesson.title}
+                            slug={lesson.slug}
+                            availableAt={new Date(lesson.availableAt)}
+                            type={lesson.lessonType}
+                        />
+                    )
+                })}
             </div>
         </div>
     )
